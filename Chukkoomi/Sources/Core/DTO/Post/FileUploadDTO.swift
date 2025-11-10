b11e19e42c64009e5a36fae6ad884420635eda6c
Sunghun Park <dfstem9@gmail.com>
2025-11-10 17:20:34 +0900

    DTO - PostRequestDTO 추가

--- Chukkoomi/Sources/Core/DTO/Post/FileUploadDTO.swift
+++ Chukkoomi/Sources/Core/DTO/Post/FileUploadDTO.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-/// 파일 업로드 DTO
-/// 요청에 대한 응답도 동일한 형태로 오기 때문에 Codable로 정의
-struct FileUploadDTO: Codable {
+/// 파일 업로드 응답 DTO
+/// multipart/form-data 는 DTO 없이 Data를 직접 업로드
+/// 서버에서 받은 파일 경로를 PostResponseDTO의 files 배열에 넣어서 게시글 생성
+struct FileUploadDTO: Decodable {
     let files: [String]
 }
---
+++ Chukkoomi/Sources/Core/DTO/Post/PostRequestDTO.swift
@@ -0,0 +1,29 @@
+//
+//  PostRequestDTO.swift
+//  Chukkoomi
+//
+//  Created by 박성훈 on 11/10/25.
+//
+
+import Foundation
+
+struct PostRequestDTO: Encodable {
+    let category: String
+    let title: String
+    let price: Int
+    let content: String
+    let value1: String
+    let value2: String
+    let value3: String
+    let value4: String
+    let value5: String
+    let value6: String
+    let value7: String
+    let value8: String
+    let value9: String
+    let value10: String
+    /// 이미 업로드가 완료된 파일들의 URL(서버 저장 경로)
+    let files: [String]
+    let longitude: Double
+    let latitude: Double
+}
