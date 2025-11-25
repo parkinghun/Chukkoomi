f90ce73754e06829011459030d04284d519511c5
Sunghun Park <dfstem9@gmail.com>
2025-11-25 16:16:14 +0900

    HomeView UI 리팩토링 및 경기 결과 더미 데이터 추가 및 적용

--- Chukkoomi/Sources/Core/Models/Match.swift
+++ Chukkoomi/Sources/Core/Models/Match.swift
@@ -7,6 +7,31 @@
 
 import Foundation
 
+// MARK: - Match Event Types
+enum MatchEventType: String, Codable, Equatable {
+    case goal = "Goal"
+    case yellowCard = "Yellow Card"
+    case redCard = "Red Card"
+}
+
+// MARK: - Match Event
+struct MatchEvent: Identifiable, Equatable, Codable {
+    let id: UUID
+    let type: MatchEventType
+    let playerName: String
+    let minute: Int
+    let teamName: String // 홈팀 or 원정팀
+
+    init(id: UUID = UUID(), type: MatchEventType, playerName: String, minute: Int, teamName: String) {
+        self.id = id
+        self.type = type
+        self.playerName = playerName
+        self.minute = minute
+        self.teamName = teamName
+    }
+}
+
+// MARK: - Match
 struct Match: Identifiable, Equatable, Codable {
     let id: Int
     let date: Date
@@ -16,4 +41,15 @@ struct Match: Identifiable, Equatable, Codable {
 //    let awayTeamLogo: String
     let homeScore: Int?
     let awayScore: Int?
+    let events: [MatchEvent]
+
+    init(id: Int, date: Date, homeTeamName: String, awayTeamName: String, homeScore: Int?, awayScore: Int?, events: [MatchEvent] = []) {
+        self.id = id
+        self.date = date
+        self.homeTeamName = homeTeamName
+        self.awayTeamName = awayTeamName
+        self.homeScore = homeScore
+        self.awayScore = awayScore
+        self.events = events
+    }
 }
